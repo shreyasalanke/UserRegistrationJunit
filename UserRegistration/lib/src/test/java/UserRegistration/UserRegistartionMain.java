c7fd80adc0e17eafebe4df35f332ceb6513087cd
shreyasalanke <shreyasalanke97@gmail.com>
2021-07-18 11:34:19 +0530

    ADDED:Program to check the entries using exception

---
+++ UserRegistration/lib/src/test/java/UserRegistration/UserRegistartionMain.java
@@ -0,0 +1,14 @@
+package UserRegistration;
+
+public class UserRegistartionMain {
+	public static void main(String[] args) 
+	 {
+		UserRegistration myObj = new UserRegistration();
+		myObj.firstNameValidate();
+		myObj.lastNameValidate();
+		myObj.emailValidate();
+		myObj.mobileValidate();
+		myObj.passwordValidate();
+	 }
+
+}
